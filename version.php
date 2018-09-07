fc1fdd144dc9423a37345c3be205f84314270e64
Megumin <Chiaki@users.noreply.github.com>
2018-09-08 02:25:15 +0300

    v0.8.4 update

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
-$cfg['version'] = "0.8.3 beta";
-$cfg['version_db'] = "2018082501";
+$cfg['version'] = "0.8.4 beta";
+$cfg['version_db'] = "2018090801";
 $cfg['vk_api_version'] = "5.73";
 
 ?>
